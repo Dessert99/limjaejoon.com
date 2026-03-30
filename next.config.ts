48974df49d72ab2c79b20104f3a31a43c89674fc
Dessert99 <lsjh1234@naver.com>
2026-03-31 01:58:44 +0900

    feat: vanilla Extract 설정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import type { NextConfig } from 'next';
 
 const withVanillaExtract = createVanillaExtractPlugin({
   unstable_turbopack: {
-    mode: 'auto',
+    mode: 'auto', // Next 버전이 16.0.0 이상일 경우에만 Turbopack 설정을 활성화
   },
 });
 
--- styles/theme.css.ts
+++ styles/theme.css.ts
@@ -1,6 +1,6 @@
-import { createGlobalTheme } from '@vanilla-extract/css';
+import { createTheme } from '@vanilla-extract/css';
 
-export const vars = createGlobalTheme(':root', {
+export const [darkTheme, vars] = createTheme({
   color: {
     bgPage: '#121212',
     bgSurface: '#121212',
@@ -17,6 +17,33 @@ export const vars = createGlobalTheme(':root', {
   shadow: {
     cardSm: '0 1px 3px rgba(0, 0, 0, 0.24)',
     cardMd: '0 10px 28px rgba(0, 0, 0, 0.36)',
+  },
+  radius: {
+    md: '0.375rem',
+    lg: '0.5rem',
+    xl: '0.75rem',
+    x2l: '1rem',
+    full: '9999px',
+  },
+});
+
+export const lightTheme = createTheme(vars, {
+  color: {
+    bgPage: '',
+    bgSurface: '',
+    bgElevated: '',
+    bgSoft: '',
+    textPrimary: '',
+    textSecondary: '',
+    textMuted: '',
+    lineSoft: '',
+    lineStrong: '',
+    accentStrong: '',
+    accentSoft: '',
+  },
+  shadow: {
+    cardSm: '',
+    cardMd: '',
   },
   radius: {
     md: '0.375rem',
