0a65d1e4a36d2b457cb04aca9689d3dfe0c166a6
Dessert99 <lsjh1234@naver.com>
2026-08-05 23:03:56 +0900

    chore: 인트로 수정

--- src/pages/home/config/intro.ts
+++ src/pages/home/config/intro.ts
@@ -2,4 +2,4 @@
 import { SITE } from '@/shared/config';
 
 /** 사이트에 처음 들어온 사람에게 타이핑으로 찍히는 한 문장 */
-export const INTRO_GREETING = `안녕하세요. ${SITE.role}, ${SITE.name}입니다.`;
+export const INTRO_GREETING = `안녕하세요. ${SITE.role} ${SITE.name}입니다.`;
